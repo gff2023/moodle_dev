0d6d70aa478027feca682c23c9cb333dc20debe7
gff2023 <125387629+gff2023@users.noreply.github.com>
2025-03-10 19:20:08 -0400

    Delete feecap/db/upgrade.php - to mirror enrol_fee

--- feecap/db/upgrade.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('MOODLE_INTERNAL') || die();
-
-function xmldb_enrol_feecap_upgrade($oldversion) {
-    if ($oldversion < 2025030900) {
-        upgrade_plugin_savepoint(true, 2025030900, 'enrol', 'feecap');
-    }
-    return true;
-}
