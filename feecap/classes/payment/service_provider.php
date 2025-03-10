27dbc3f686e0818ee06d9ae1eaf777c776a57cc5
gff2023 <125387629+gff2023@users.noreply.github.com>
2025-03-10 18:39:56 -0400

    Update service_provider.php to mirror enrol_fee

--- feecap/classes/payment/service_provider.php
+++ feecap/classes/payment/service_provider.php
@@ -1,42 +1,38 @@
 <?php
 namespace enrol_feecap\payment;
 
-defined('MOODLE_INTERNAL') || die();
 
 class service_provider implements \core_payment\local\callback\service_provider {
 
     public static function get_payable(string $component, string $paymentarea, int $instanceid): \core_payment\local\entities\payable {
         global $DB;
 
-        $enrol = $DB->get_record('enrol', ['enrol' => 'feecap', 'id' => $instanceid], '*', MUST_EXIST);
+        $instance = $DB->get_record('enrol', ['enrol' => 'feecap', 'id' => $instanceid], '*', MUST_EXIST);
 
-        $cost = (float)$enrol->cost;
-        if ($cost <= 0) {
-            $cost = (float)get_config('enrol_feecap', 'cost');
-        }
-        if ($cost < 0) {
-            $cost = 0;
-        }
-
-        return new \core_payment\local\entities\payable($cost, $enrol->currency, $enrol->customint1);
+        return new \core_payment\local\entities\payable($instance->cost, $instance->currency, $instance->customint1);
     }
 
-    public static function get_success_url(string $component, int $paymentarea): \moodle_url {
+    public static function get_success_url(string $paymentarea, int $instanceid): \moodle_url {
         global $DB;
-        $courseid = $DB->get_field('enrol', 'courseid', ['enrol' => 'feecap', 'id' => $paymentarea], MUST_EXIST);
+        $courseid = $DB->get_field('enrol', 'courseid', ['enrol' => 'feecap', 'id' => $instanceid], MUST_EXIST);
         return new \moodle_url('/course/view.php', ['id' => $courseid]);
     }
 
-    public static function deliver_order(string $component, string $paymentarea, int $instanceid, int $userid): bool {
+    public static function deliver_order(string $paymentarea, int $instanceid, int $paymentid, int $userid): bool {
         global $DB;
 
         $instance = $DB->get_record('enrol', ['enrol' => 'feecap', 'id' => $instanceid], '*', MUST_EXIST);
         $plugin = enrol_get_plugin('feecap');
 
-        $roleid = $instance->roleid ?: $plugin->get_config('roleid');
-        $timeend = $instance->enrolperiod ? (time() + $instance->enrolperiod) : 0;
+        if ($instance->enrolperiod) {
+            $timestart = time();
+            $timeend   = $timestart + $instance->enrolperiod;
+        } else {
+            $timestart = 0;
+            $timeend   = 0;
+        }
 
-        $plugin->enrol_user($instance, $userid, $roleid, time(), $timeend);
+        $plugin->enrol_user($instance, $userid, $instance->roleid, $timestart, $timeend);
         return true;
     }
 }
