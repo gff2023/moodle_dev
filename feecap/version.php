a1f8be9953b3a56646888002799cc146974f0f8d
gff2023 <125387629+gff2023@users.noreply.github.com>
2025-03-10 18:32:32 -0400

    Update version.php to mirror enrol_fee

--- feecap/version.php
+++ feecap/version.php
@@ -1,8 +1,6 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'enrol_feecap';
 $plugin->version = 2025030900; // YYYYMMDDXX.
-$plugin->requires = 2022041900; // Moodle 4.0 minimum (adjust if needed).
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0';
+$plugin->requires = 2024100100; // Moodle 4.0 minimum (adjust if needed).
+$plugin->component = 'enrol_feecap';
